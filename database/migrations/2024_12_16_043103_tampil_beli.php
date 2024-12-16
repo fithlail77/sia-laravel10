07952a0a99aefd33566135e4bf430c1ac7df30c1
fithlail77 <fithlail@gmail.com>
2024-12-16 14:02:47 +0700

    View Sql with migration

---
+++ database/migrations/2024_12_16_043103_tampil_beli.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared('
+            CREATE VIEW tampil_pembelian AS 
+            (SELECT 
+                barang.kd_brg AS kd_brg,
+                detail_pembelian.no_beli AS no_beli,
+                barang.nm_brg AS nm_brg,
+                barang.harga AS harga,
+                detail_pembelian.qty_beli AS qty_beli 
+            FROM (barang join detail_pembelian) 
+            WHERE barang.kd_brg = detail_pembelian.kd_brg);
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared('DROP VIEW IF EXISTS tampil_pembelian');
+    }
+};
